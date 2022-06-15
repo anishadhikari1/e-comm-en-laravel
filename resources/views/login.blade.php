ef821c7c46159f94a69717d6347bee8b1855ed57
anishadhikari1 <anishadk55@gmail.com>
2022-06-15 09:31:01 +0545

    complete login module

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,17 +7,18 @@
 <div class="row">
     <div class="col-sm-4">
         <h2>Login Page</h2>
-    <form>
+    <form action="login" method="post" >
+      @csrf
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="InputEmail1">Email address</label>
+    <input type="email" name="email" class="form-control" id="InputEmail1" placeholder="Enter email">
+  
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" name="password" class="form-control" id="InputPassword1" placeholder="Password">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Login</button>
 </form>
     </div>
     </div>
